d20f1f501150940dd08b561ba1c362b7bb2a12a7
oid85 <oid85@mail.ru>
2025-01-24 21:55:37 +0700

    dev

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
+import os
 
 
 def do():
-    while True:
-        time.sleep(30)
+    cmd = "git --version"
+    returned_value = os.system(cmd)  # returns the exit code in unix
+    print('returned value:', returned_value)
 
 
 if __name__ == '__main__':
